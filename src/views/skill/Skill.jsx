7a83c16b6b7eb23326b4e55d90ebe86b26cfb216
Sem-Phanun <phanunsem097@gmail.com>
2024-02-12 23:50:39 +0700

    committing

--- src/views/skill/Skill.jsx
+++ src/views/skill/Skill.jsx
@@ -1,58 +1,19 @@
 import "./skill.scss";
-
+import NodeJs from '../../assets/image/nodejs.png'
 const Skill = () => {
   return (
     <>
-      <main className="skill___container">
-        <article className="title-skill">
-          <h1>Explore my skill</h1>
+      <section className="skill___container">
+        <article className="title-wrapper">
+          <h1 className="title">Skill</h1>
         </article>
 
-        <div className="skill-wrapper">
+        <main className="skill-wrapper">
           <li>
-            <h3>JavaScript</h3>
-            <span className="bar">
-              <span className="javascript"></span>
-            </span>
+            <img src={NodeJs} alt="" />
           </li>
-          <li>
-            <h3>React.Js</h3>
-            <span className="bar">
-              <span className="react"></span>
-            </span>
-          </li>
-          <li>
-            <h3>Node.Js, Express.Js</h3>
-            <span className="bar">
-              <span className="node"></span>
-            </span>
-          </li>
-          <li>
-            <h3>Web Design</h3>
-            <span className="bar">
-              <span className="webdesign"></span>
-            </span>
-          </li>
-          <li>
-            <h3>Relational Database</h3>
-            <span className="bar">
-              <span className="database"></span>
-            </span>
-          </li>
-          <li>
-            <h3>Java</h3>
-            <span className="bar">
-              <span className="java"></span>
-            </span>
-          </li>
-          <li>
-            <h3>Spring Core</h3>
-            <span className='bar'>
-              <span className='springcore'></span>
-            </span>
-          </li>
-        </div>
-      </main>
+        </main>
+      </section>
     </>
   );
 };
