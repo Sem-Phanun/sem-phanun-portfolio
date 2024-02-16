e0acc8dd54b8b7d4b9b1c4c4f9bc9fc483761bce
Sem-Phanun <phanunsem097@gmail.com>
2024-02-16 23:07:19 +0700

    custom SEO

--- src/views/contact/Contact.jsx
+++ src/views/contact/Contact.jsx
@@ -1,47 +1,50 @@
 import { MdEmail } from "react-icons/md";
 import { FaLinkedin } from "react-icons/fa";
+import Layout from "../../components/layout/Layout";
 import "./contact.scss";
 const Contact = () => {
   return (
     <>
-      <section className="contact-container">
-        <article className="titleHeader">
-          <h1>Contact Me</h1>
-          <p>Get in touch with me.</p>
-        </article>
-        <main className="content">
-          <form className="contact-form">
-            <div className="form-group">
-              <label htmlFor="">Name</label>
-              <input
-                type="text"
-                className="form-control"
-                id="name"
-                placeholder="name"
-              />
-            </div>
-            <div className="form-group">
-              <label htmlFor="email">Email</label>
-              <input
-                type="email"
-                className="form-control"
-                id="email"
-                placeholder="email"
-              />
-            </div>
-            <div className="form-group">
-              <label htmlFor="message">Message</label>
-              <textarea className="form-control" id="message" rows="3"></textarea>
-            </div>
-            <button type="submit" className="btn btn-primary">
-              Submit
-            </button>
-          </form>
-          {/* <li className="box">
-            <a href="mailto:phanunsem@gmail.com"><MdEmail className="contact-icons"/>phanunsem@gmail.com</a>
-          </li> */}
-        </main>
-      </section>
+      <Layout title={"CONTACT US"}>
+        <section className="contact-container">
+          <article className="titleHeader">
+            <h1>Contact Me</h1>
+            <p>Get in touch with me.</p>
+          </article>
+          <main className="content">
+            <form className="contact-form">
+              <div className="form-group">
+                <label htmlFor="">Name</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  id="name"
+                  placeholder="name"
+                />
+              </div>
+              <div className="form-group">
+                <label htmlFor="email">Email</label>
+                <input
+                  type="email"
+                  className="form-control"
+                  id="email"
+                  placeholder="email"
+                />
+              </div>
+              <div className="form-group">
+                <label htmlFor="message">Message</label>
+                <textarea className="form-control" id="message" rows="3"></textarea>
+              </div>
+              <button type="submit" className="btn btn-primary">
+                Submit
+              </button>
+            </form>
+            {/* <li className="box">
+              <a href="mailto:phanunsem@gmail.com"><MdEmail className="contact-icons"/>phanunsem@gmail.com</a>
+            </li> */}
+          </main>
+        </section>
+      </Layout>
     </>
   );
 };
--- src/views/skill/Skill.jsx
+++ src/views/skill/Skill.jsx
@@ -5,49 +5,49 @@ const Skill = () => {
   return (
     <>
       <Layout title={"SKILL - PORTFOLIO"}>
-      <section className="skill___container">
-        <article className="title-wrapper">
-          <h1 className="title">Skill</h1>
-        </article>
+        <section className="skill___container">
+          <article className="title-wrapper">
+            <h1 className="title">Skill</h1>
+          </article>
 
-        <article className="pro-box">
-          <h3 className="pro-title">Proficient: </h3>
-        </article>
-        <main className="skill-wrapper">
-          {skill.map((item, index) => {
-            return (
-              <figure className="image-box" key={index}>
-                <img src={item.image} alt={item.name} />
-              </figure>
-            );
-          })}
-        </main>
-        <article className="pro-box">
-          <h3 className="pro-title">Fimilar:</h3>
-        </article>
-        <main className="skill-wrapper">
-          {fimilarSkill.map((item, index) => {
-            return (
-              <figure className="image-box" key={index}>
-                <img src={item.image} alt="" />
-              </figure>
-            );
-          })}
-        </main>
+          <article className="pro-box">
+            <h3 className="pro-title">Proficient: </h3>
+          </article>
+          <main className="skill-wrapper">
+            {skill.map((item, index) => {
+              return (
+                <figure className="image-box" key={index}>
+                  <img src={item.image} alt={item.name} />
+                </figure>
+              );
+            })}
+          </main>
+          <article className="pro-box">
+            <h3 className="pro-title">Fimilar:</h3>
+          </article>
+          <main className="skill-wrapper">
+            {fimilarSkill.map((item, index) => {
+              return (
+                <figure className="image-box" key={index}>
+                  <img src={item.image} alt="" />
+                </figure>
+              );
+            })}
+          </main>
 
-        <article className="pro-box">
-          <h3 className="pro-title">Tools:</h3>
-        </article>
-        <main className="skill-wrapper">
-          {tools.map((item, index) => {
-            return (
-              <figure className="image-box" key={index}>
-                <img src={item.image} alt={item.name} />
-              </figure>
-            );
-          })}
-        </main>
-      </section>
+          <article className="pro-box">
+            <h3 className="pro-title">Tools:</h3>
+          </article>
+          <main className="skill-wrapper">
+            {tools.map((item, index) => {
+              return (
+                <figure className="image-box" key={index}>
+                  <img src={item.image} alt={item.name} />
+                </figure>
+              );
+            })}
+          </main>
+        </section>
       </Layout>
     </>
   );
